43efb4e26cbd5aefbe9949c72b0e1a878706080e
Aditya-Singh714 <adityajaat0000@gmail.com>
2024-06-05 12:20:48 +0530

    implement left sidebar, homepage_layout, convex_setup, clerk_setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://discrete-malamute-59.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+};
+
+export default authConfig;
